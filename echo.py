2355c25edbc2ac6a4a4661c9e6643545adc8dc22
Mark T. Holder <mtholder@gmail.com>
2010-01-22 14:16:09 -0600

    disastrous lecture 1

---
+++ echo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys
+print "Number of command line arguments = %d" % (len(sys.argv) - 1)
+for o, a in enumerate(sys.argv[1:]):
+    print 'Arg %d = "%s"' % (1 + o, a)
+print "End of arguments"
+
