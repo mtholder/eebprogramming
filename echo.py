ca2a5dffe279da2b0e7ea344e2693794d633c6a9
Mark T. Holder <mtholder@gmail.com>
2010-01-27 14:33:32 -0600

    lec2

--- echo.py
+++ echo.py
@@ -4,4 +4,4 @@ print "Number of command line arguments = %d" % (len(sys.argv) - 1)
 for o, a in enumerate(sys.argv[1:]):
     print 'Arg %d = "%s"' % (1 + o, a)
 print "End of arguments"
-
+    
---
+++ lec2pythonbasics/lecture.py
@@ -0,0 +1,28 @@
+5 + 5
+5 + 2.3
+3*2
+3.0*2
+# strings
+"hello"
+'hello, there world!'
+print 'hello, there world'
+print 6
+"hi"*3
+"hi" + 5
+#types
+type(3)
+type(3.0)
+type("hi")
+#variables
+radius = 2
+circum = 2*radius*3.141592653589793
+circum
+radius = 4
+circum
+#functions
+def calc_circumference(radius):
+    return 2*radius*3.141592653589793
+calc_circumference(radius)
+calc_circumference(2)
+calc_circumference(4)
+
