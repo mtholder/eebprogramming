634c70fc03a17739d6c01aef18aaa7e67585f21f
Mark T. Holder <mtholder@gmail.com>
2010-01-29 10:14:30 -0600

    initialize  upper_limit from command line arguments

--- lec2pythonbasics/num_attachments.py
+++ lec2pythonbasics/num_attachments.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import sys
+
 output_stream = open('a', 'w')
-upper_limit = 101
+
+upper_limit = int(sys.argv[1])
+
 def calc_num_attachment_points(n):
     return 2*n - 3
 for i in range(3, upper_limit):
