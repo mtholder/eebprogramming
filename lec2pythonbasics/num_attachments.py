3a23b4414db73628c1e3d0d621d3a3897bd8a9cb
Mark T. Holder <mtholder@gmail.com>
2010-01-29 10:16:19 -0600

    takes output filename  from command line arguments, or uses stdandard out

--- lec2pythonbasics/num_attachments.py
+++ lec2pythonbasics/num_attachments.py
@@ -4,8 +4,11 @@ import sys
 
 upper_limit = int(sys.argv[1])
 
-filename = sys.argv[2]
-output_stream = open(filename, 'w')
+if len(sys.argv) == 3:
+    filename = sys.argv[2]
+    output_stream = open(filename, 'w')
+else:
+    output_stream = sys.stdout
 
 
 def calc_num_attachment_points(n):
