1be33051771aadfd6b586df7f7c223ce9e3119b7
Mark T. Holder <mtholder@gmail.com>
2010-01-29 10:12:42 -0600

    simplest num_attachments -- writes to a file

--- lec2pythonbasics/num_attachments.py
+++ lec2pythonbasics/num_attachments.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python
-import sys
-program_name = sys.argv[0]
-if len(sys.argv) > 3 or len(sys.argv) < 2:
-    sys.exit(program_name + ": Expecting two arguments: <num_taxa> [filename]")
-
-upper_limit = int(sys.argv[1])
-
-if len(sys.argv) == 3:
-    filename = sys.argv[2]
-    output_stream = open(filename, 'w')
-else:
-    output_stream = sys.stdout
-
+output_stream = open('a', 'w')
 def calc_num_attachment_points(n):
     return 2*n - 3
-for i in range(3, upper_limit):
+for i in range(3, 101):
     num_attachments = calc_num_attachment_points(i)
     output_stream.write(str(i) + " " + str(num_attachments) + "\n")
 
