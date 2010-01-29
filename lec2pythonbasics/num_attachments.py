093c4ea516610023d4b6bd5ea809e6e1542f02b1
Mark T. Holder <mtholder@gmail.com>
2010-01-29 10:13:42 -0600

    slightly better - uses a variable for the upper_limit

--- lec2pythonbasics/num_attachments.py
+++ lec2pythonbasics/num_attachments.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 output_stream = open('a', 'w')
+upper_limit = 101
 def calc_num_attachment_points(n):
     return 2*n - 3
-for i in range(3, 101):
+for i in range(3, upper_limit):
     num_attachments = calc_num_attachment_points(i)
     output_stream.write(str(i) + " " + str(num_attachments) + "\n")
 
