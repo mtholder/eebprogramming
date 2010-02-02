604b88caaebb58c30fcdce848e7765cb8d0b94ed
Mark T. Holder <mtholder@gmail.com>
2010-02-02 13:43:24 -0600

    end of lec2, beginning of lecture 3

--- lec2pythonbasics/lecture.py
+++ lec2pythonbasics/lecture.py
@@ -213,7 +213,6 @@ while True:
     if x == 'N' or x == 'NO':
         break
 
-
 def calc_num_attachment_points(n):
     return 2*n - 3
 
@@ -221,6 +220,8 @@ for i in range(3, 101):
     num_attachments = calc_num_attachment_points(i)
     print i, num_attachments
 
+print "Mark, go to the exercise now!"
+#dict
 genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
 genetic_code['UUU']
 genetic_code['UGA']
@@ -229,7 +230,56 @@ for key, value in genetic_code.iteritems():
     print key, "-->", value
 
 'AUG' in genetic_code
+'F' in genetic_code
+print genetic_code['UUU']
 "not a codon" in genetic_code
 genetic_code[10] = 'strange'
 genetic_code.keys()
 genetic_code.values()
+del genetic_code[10]
+print genetic_code
+genetic_code[10] = 'first value'
+genetic_code[11] = 'first value'
+print genetic_code[10]
+print genetic_code[11]
+genetic_code[10] = 'second value'
+print genetic_code[10]
+print genetic_code[11]
+for k, v in genetic_code.iteritems():
+    if v == 'V':
+        print k
+
+reverse_genetic_code = {'V' : ['GUC', 'GUA', 'GUG', 'GUU']}
+for codon, amino_acid in genetic_code.iteritems():
+    codon_list = []
+    for k, v in genetic_code.iteritems():
+        if v == amino_acid:
+            codon_list.append(k)
+    reverse_genetic_code[amino_acid] = codon_list
+
+for k, v in reverse_genetic_code.iteritems():
+    print k, "<--", v
+
+x = set()
+x.add(1)
+print x
+x.add(5)
+print x
+1 in x
+2 in x
+5 in x
+x.add(5)
+print x
+x.remove(5)
+print x
+print "Mark, go to the assignment now!"
+#factorial
+print "Factorial"
+def factorial(x):
+    if isinstance(x, float):
+        raise TypeError("factorial function is only defined for integers")
+    product = 1
+    for i in range(1, x + 1):
+        product = product*i
+    return product
+
