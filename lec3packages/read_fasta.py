23cc9f8f588b5683066eded09f7dfb632a1c4298
Mark T. Holder <mtholder@gmail.com>
2010-02-09 04:53:18 -0600

    reorganizing code so that we could use GenBankSequence via an import -- we moved the command line execution to an if __name__ == '__main__' block of code

--- lec3packages/read_fasta.py
+++ lec3packages/read_fasta.py
@@ -5,52 +5,6 @@
 import sys
 import re
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- a filename")
-inp = open(sys.argv[1], 'rU')
-
-identifiers = []
-sequences = []
-current_seq = []
-
-for line in inp:
-    stripped = line.strip()
-    if line.startswith('>'):
-        if current_seq:
-            sequences.append(''.join(current_seq))
-        identifiers.append(stripped[1:])
-        current_seq = []
-    else:
-        if stripped:
-            current_seq.append(stripped)
-if current_seq:
-    sequences.append(''.join(current_seq))
-
-assert(len(identifiers) == len(sequences))
-
-first_part = r'gi\|(\d+)\|gb\|([a-zA-Z0-9.]+)'
-second_part = r'\|\s*(\S+\s+\S+)\s+(\S.*\S)'
-third_part = r',\s*(\S.*\S)\s*'
-pattern = first_part + second_part + third_part
-identifier_pattern = re.compile(pattern)
-
-gi_numbers = []
-accession_numbers = []
-locus_list = []
-species_list = []
-for element in identifiers:
-    match_object = identifier_pattern.match(element)
-    if match_object:
-        gi, acc, sp, locus, blah =  match_object.groups()
-        print [gi, acc, sp, locus, blah]
-        gi_numbers.append(gi)
-        accession_numbers.append(acc)
-        species_list.append(sp)
-        locus_list.append(locus)
-    else:
-        print element, "does not match our search pattern"
-
-
 class GenBankSequence(object):
     "Encapsulates the information that is contained in a genbank record."
 
@@ -89,21 +43,70 @@ class GenBankSequence(object):
         for i in a:
             b.append(rc_dict[i])
         self.sequence = ''.join(b)
-seq_objects = []
-for index, element in enumerate(gi_numbers):
-    o = GenBankSequence(gi=element,
-                        accession=accession_numbers[index],
-                        species=species_list[index],
-                        locus=locus_list[index],
-                        sequence=sequences[index])
-    seq_objects.append(o)
-    print
-    print str(o)
-    o.reverse_and_complement()
-    print
-    print o
-    o.reverse_and_complement()
-    print
-    print o
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- a filename")
+    inp = open(sys.argv[1], 'rU')
+
+    identifiers = []
+    sequences = []
+    current_seq = []
+
+    for line in inp:
+        stripped = line.strip()
+        if line.startswith('>'):
+            if current_seq:
+                sequences.append(''.join(current_seq))
+            identifiers.append(stripped[1:])
+            current_seq = []
+        else:
+            if stripped:
+                current_seq.append(stripped)
+    if current_seq:
+        sequences.append(''.join(current_seq))
+
+    assert(len(identifiers) == len(sequences))
+
+    first_part = r'gi\|(\d+)\|gb\|([a-zA-Z0-9.]+)'
+    second_part = r'\|\s*(\S+\s+\S+)\s+(\S.*\S)'
+    third_part = r',\s*(\S.*\S)\s*'
+    pattern = first_part + second_part + third_part
+    identifier_pattern = re.compile(pattern)
+
+    gi_numbers = []
+    accession_numbers = []
+    locus_list = []
+    species_list = []
+    for element in identifiers:
+        match_object = identifier_pattern.match(element)
+        if match_object:
+            gi, acc, sp, locus, blah =  match_object.groups()
+            print [gi, acc, sp, locus, blah]
+            gi_numbers.append(gi)
+            accession_numbers.append(acc)
+            species_list.append(sp)
+            locus_list.append(locus)
+        else:
+            print element, "does not match our search pattern"
+
+
+    seq_objects = []
+    for index, element in enumerate(gi_numbers):
+        o = GenBankSequence(gi=element,
+                            accession=accession_numbers[index],
+                            species=species_list[index],
+                            locus=locus_list[index],
+                            sequence=sequences[index])
+        seq_objects.append(o)
+        print
+        print str(o)
+        o.reverse_and_complement()
+        print
+        print o
+        o.reverse_and_complement()
+        print
+        print o
 
 
