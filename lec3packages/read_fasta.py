db3a810edad8bdcdf95925754239e5944160f0fc
Mark T. Holder <mtholder@gmail.com>
2010-02-09 03:45:12 -0600

    initial read_fasta.py

---
+++ lec3packages/read_fasta.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"An example of simple re parsing"
+
+
+import sys
+if len(sys.argv) != 2:
+    sys.exit(sys.argv[0] + ": Expecting one command line argument -- a filename")
+inp = open(sys.argv[1], 'rU')
+
+identifiers = []
+sequences = []
+
+for line in inp:
+    if line.startswith('>'):
+        identifiers.append(line.strip()[1:])
+
+print '\n'.join(identifiers)
+
