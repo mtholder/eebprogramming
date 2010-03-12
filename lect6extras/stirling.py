794f6378755e73e92852e0340e58095a6dab7e83
Mark T. Holder <mtholder@gmail.com>
2010-03-12 10:50:33 -0600

    stirling number calc with caching

--- lect6extras/stirling.py
+++ lect6extras/stirling.py
@@ -5,13 +5,19 @@ def fact(x):
        return 1
    return x * fact(x-1)
 
+_STORED = {}
 def stirling(n, k):
+   key = (n,k)
+   r = _STORED.get(key)
+   if r is not None:
+       return r
    #print n, k
    if n == k:
        return 2
    if k == 1:
        return fact(n-1)
    r = stirling(n-1, k-1) + (n-1)*stirling(n-1, k)
+   _STORED[key] = r
    return r
 
 n, k = int(sys.argv[1]), int(sys.argv[2])
