6c25be98c4c2017db366860d8e696f2cec28c182
Mark T. Holder <mtholder@gmail.com>
2010-04-02 09:07:58 -0500

    refactored to simplify and allow multiple trees to be scored

--- lect7dendropy/sankoff.py
+++ lect7dendropy/sankoff.py
@@ -58,9 +58,28 @@ def sankoff(postorder_node_list, step_matrix, taxa_to_state_set_map):
                 score = 0
                 for pattern_index in xrange(num_patterns):
                     score += min(nd.char_costs[pattern_index])
-        #print nd.char_costs
     return score
 
+def score_tree(tree, taxa_to_states, step_matrix=None):
+    if not tree.is_rooted:
+        raise ValueError("Tree must be rooted")
+    if step_matrix is None:
+        step_matrix =  [ [0, 1, 1, 1],
+                [1, 0, 1, 1],
+                [1, 1, 0, 1],
+                [1, 1, 1, 0],
+            ]
+    root = tree.seed_node
+    root_children = root.child_nodes()
+    if len(root_children) != 2:
+        raise ValueError("Expecting a binary rooted tree.  Root has more than 2 children!")
+    node_list = [i for i in tree.postorder_node_iter()]
+    for nd in node_list:
+        c = nd.child_nodes()
+        if c:
+            if len(c) != 2:
+                raise ValueError("Tree must be fully resolved")
+    return sankoff(node_list, step_matrix=step_matrix, taxa_to_state_set_map=taxa_to_states)
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -69,10 +88,6 @@ if __name__ == '__main__':
     if len(args) == 0:
         sys.exit("Expecting a filename as an argument")
 
-    char_index = 0
-    char_mat_index = 0
-    taxon_set_index = 0
-    tree_list_index = 0
     tree_index = 0
 
     try:
@@ -86,47 +101,28 @@ if __name__ == '__main__':
             if len(dataset.taxon_sets) != 1:
                 raise ValueError("Expecting one set of taxa in %s" % f)
             if len(dataset.tree_lists) != 1:
-                raise ValueError("Expecting one tree in %s" % f)
+                raise ValueError("Expecting one tree block in %s" % f)
             if len(dataset.char_matrices) != 1:
                 raise ValueError("Expecting one character matrix in %s" % f)
-            char_mat = dataset.char_matrices[char_mat_index]
+            char_mat = dataset.char_matrices[0]
             num_char = len(char_mat[0])
-            taxon_set = dataset.taxon_sets[taxon_set_index]
-            tree = dataset.tree_lists[tree_list_index][tree_index]
-            p_score = 0
-            state_alphabet = char_mat.state_alphabets[0]
-            taxon_to_state_indices = char_mat.create_taxon_to_state_set_map()
-
-            if not tree.is_rooted:
-                raise ValueError("Tree must be rooted")
-
-            root = tree.seed_node
-            root_children = root.child_nodes()
-            if len(root_children) != 2:
-                raise ValueError("Expecting a binary rooted tree.  Root has more than 2 children!")
-
-
-            node_list = [i for i in tree.postorder_node_iter()]
-            for nd in node_list:
-                c = nd.child_nodes()
-                if c:
-                    if len(c) != 2:
-                        raise ValueError("Tree must be fully resolved")
-            s_mat = [   [0, 1, 1, 1],
-                        [1, 0, 1, 1],
-                        [1, 1, 0, 1],
-                        [1, 1, 1, 0],
-                    ]
-            s_mat = [   [0, 5, 1, 5],
-                        [5, 0, 5, 1],
-                        [1, 5, 0, 5],
-                        [5, 1, 5, 0],
-                    ]
-            p_score = sankoff(node_list, step_matrix=s_mat, taxa_to_state_set_map=taxon_to_state_indices)
-            print p_score
-            sys.exit(0)
+            taxon_set = dataset.taxon_sets[0]
+            taxon_to_state_set = char_mat.create_taxon_to_state_set_map()
+            for n, tree in enumerate(dataset.tree_lists[0]):
+                p_score = score_tree(tree, taxon_to_state_set)
+                print "Tree", n+1, p_score
     except Exception as x:
         if _DEBUGGING:
             raise
         sys.exit(str(x))
 
+
+
+
+
+x='''    s_mat = [   [0, 5, 1, 5],
+                [5, 0, 5, 1],
+                [1, 5, 0, 5],
+                [5, 1, 5, 0],
+            ]
+'''
